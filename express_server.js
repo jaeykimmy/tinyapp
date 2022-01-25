06b163dc4be3d3a3f7d3dd00dccfd23cb48832e1
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-01-25 17:34:30 -0500

    cookies login logout implemented with branch

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const generateRandomString = function() {
@@ -20,6 +22,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.post('/logout', (req, res) => {
+  console.log(req.body.username);
+  res.clearCookie('username', req.body.username);
+  res.redirect(`/urls`);
+});
+
+app.post('/login', (req, res) => {
+  console.log(req.body.username);
+  res.cookie('username', req.body.username);
+  res.redirect(`/urls`);
+});
+
 app.post("/urls", (req, res) => {
   let randomID = generateRandomString();
   urlDatabase[randomID] = req.body.longURL;
@@ -32,8 +46,8 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  console.log(urlDatabase[req.params.shortURL]);
-  console.log(req.body.longURL);
+  //console.log(urlDatabase[req.params.shortURL]);
+  //console.log(req.body.longURL);
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect(`/urls/${req.params.shortURL}`);
 });
@@ -45,18 +59,27 @@ app.get("/u/:shortURL", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies['username'],
+    urls: urlDatabase
+  };
+  res.render("urls_new",templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies['username'],
+    urls: urlDatabase
+  };
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies['username'],
+    urls: urlDatabase
   };
   res.render("urls_show", templateVars);
 });
