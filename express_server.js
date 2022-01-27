f4f722f8dc85fe27f09a063a28bb13bbd436393f
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-01-27 16:30:24 -0500

    made sure json had all the dependencies needed to work properly

--- express_server.js
+++ express_server.js
@@ -2,13 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-//const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const cookieSession = require('cookie-session');
 const { restart } = require("nodemon");
 const {generateRandomString, getUserByEmail, urlsForUser} = require('./helpers');
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(cookieSession({
   name: 'session',
